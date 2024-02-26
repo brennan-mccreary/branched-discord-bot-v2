3362e9563a2e8e54ecde65a21b8484e612eecf0e
Brennan McCreary <brennanmccreary@gmail.com>
2024-02-26 17:26:40 -0500

    Add moderation tools and commands.

---
+++ src/commands/tools/moderation/kick.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Kick member')
+        .addUserOption(option => (
+                option
+                    .setName('target')
+                    .setDescription('Member to kick')
+                    .setRequired(true)
+            ))
+        .addStringOption(option => (
+                option
+                    .setName('reason')
+                    .setDescription("The reason for kicking member.")
+            )),
+    async execute(interaction, client) {
+        const user = interaction.options.getUser('target');
+        let reason = interaction.options.getString('reason') 
+        const member = await interaction.guild.members
+            .fetch(user.id)
+            .catch(console.error)
+
+        if (!reason) reason = "No reason provided.";
+
+        await member.kick(reason).catch(console.error);
+
+        await interaction.reply({
+            content: `Kicked ${user.tag}.`,
+            ephemeral: true
+        })
+    }
+}
