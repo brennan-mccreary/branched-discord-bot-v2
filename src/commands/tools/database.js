6f5ead118d4031f02035a8f5e200bbba6bc89cca
Brennan McCreary <brennanmccreary@gmail.com>
2024-12-14 18:11:46 -0500

    Change application commands to global for multi-guild utilization.

--- src/commands/tools/database.js
+++ src/commands/tools/database.js
@@ -1,38 +1,38 @@
-const Guild = require('../../schemas/guild');
-const { SlashCommandBuilder } = require('discord.js');
-const mongoose = require('mongoose');
+const Guild = require("../../schemas/guild");
+const { SlashCommandBuilder } = require("discord.js");
+const mongoose = require("mongoose");
+
+//Future notes:
+//Change reply to embed
+//Change command name to get guild info
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('database')
-        .setDescription('Returns information from the database'),
-    async execute(interaction, client) {
-        // console.log(interaction.guild)
-        console.log(client.guilds.cache.get('1121179513947181160'))
+  data: new SlashCommandBuilder()
+    .setName("database")
+    .setDescription("Returns information from the database"),
+  async execute(interaction, client) {
 
-        let guildProfile = await Guild.findOne({ guildId: interaction.guild.id });
-        if(!guildProfile) {
-            guildProfile = await new Guild({
-                _id: new mongoose.Types.ObjectId(),
-                guildId: interaction.guild.id,
-                guildName: interaction.guild.name,
-                guildIcon: interaction.guild.iconURL() ? interaction.guild.iconURL() : "none"
-            });
+    let guildProfile = await Guild.findOne({ guildId: interaction.guild.id });
+    if (!guildProfile) {
+        guildProfile = await new Guild({
+        _id: new mongoose.Types.ObjectId(),
+        guildId: interaction.guild.id,
+        guildName: interaction.guild.name,
+        guildIcon: interaction.guild.iconURL()
+          ? interaction.guild.iconURL()
+          : "none",
+      });
 
-        await guildProfile.save().catch(console.error);
-        await interaction.reply({
-            content: `Server Name: ${guildProfile.guildName}`
-        })
-        
-      
-
-        } else {
-            await guildProfile.save().catch(console.error);
-            await interaction.reply({
-                content: `Server Id: ${guildProfile.guildId}\nServer Icon: ${guildProfile.guildIcon}`
-            })
-            // console.log(guildProfile);
-        }
-
+      await guildProfile.save().catch(console.error);
+      await interaction.reply({
+        content: `Server Name: ${guildProfile.guildName}`,
+      });
+    } else {
+      await guildProfile.save().catch(console.error);
+      await interaction.reply({
+        content: `Server Id: ${guildProfile.guildId}\nServer Icon: ${guildProfile.guildIcon}\nServer Name: ${guildProfile.guildName}`,
+      });
+      // console.log(guildProfile);
     }
-}
+  },
+};
--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,41 +1,51 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const fs = require('fs');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const fs = require("fs");
+const chalk = require('chalk');
 
 module.exports = (client) => {
-    client.handleCommands = async () => {
-        const commandFolders = fs.readdirSync('./src/commands');
-        for (const folder of commandFolders) {
-            const commandFiles = fs
-                .readdirSync(`./src/commands/${folder}`)
-                .filter((file) => file.endsWith(".js"));
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
 
+      const { commands, commandArray } = client;
+      for (const file of commandFiles) {
+        const command = require(`../../commands/${folder}/${file}`);
 
-            const { commands, commandArray } = client;
-            for (const file of commandFiles) {
-                const command = require(`../../commands/${folder}/${file}`);
+        commands.set(command.data.name, command);
+        commandArray.push(command.data.toJSON());
 
-                commands.set(command.data.name, command);
-                commandArray.push(command.data.toJSON());
+        console.log(
+          `Command: ${command.data.name} has been passed through the handler`
+        );
+      }
+    }
 
-                console.log(`Command: ${command.data.name} has been passed through the handler`);
-            }
-        }
+    const clientId = "1077326128374628423";
+    // const guildId = "1121179513947181160";
 
-        const clientId = '1077326128374628423';
-        const guildId = '1121179513947181160';
+    const rest = new REST({ version: "9" }).setToken(
+      process.env.DISCORD_BOT_TOKEN
+    );
 
-        const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_BOT_TOKEN);
-        try {
-            console.log('Started refreshing application (/) commands.');
+    try {
+      console.log("Started refreshing application (/) commands.");
 
-            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-                body: client.commandArray,
-            });
+      await rest.put(Routes.applicationCommands(clientId), {
+        body: client.commandArray,
+      });
 
-            console.log('Successfully reloaded application (/) commands.');
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      //For single guild use:
+      //   await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      //     body: client.commandArray,
+      //   });
+
+      console.log(chalk.green("Successfully reloaded application (/) commands."));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 };
