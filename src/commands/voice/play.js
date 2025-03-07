db5b5a21bfdf53e8e4c21713e270ca60da359c37
Brennan McCreary <brennanmccreary@gmail.com>
2025-03-06 21:08:53 -0500

    Add play and leave commands as base for playing music from bot in the future.

--- src/commands/voice/play.js
+++ src/commands/voice/play.js
@@ -1,10 +1,41 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+} = require("@discordjs/voice");
+const ytdl = require("@distube/ytdl-core");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('play')
-        .setDescription('Plays sound'),
-    async execute(interaction, client) {
-        
+  data: new SlashCommandBuilder().setName("play").setDescription("Plays sound"),
+  async execute(interaction, client) {
+    try {
+      const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      const voiceChannel = interaction.member.voice.channel;
+
+      if (!voiceChannel) {
+        return interaction.reply("❌ You need to be in a voice channel!");
+      }
+
+      const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+      });
+
+      const stream = ytdl(url, {
+        filter: "audioonly",
+        quality: "highestaudio",
+      });
+      const resource = createAudioResource(stream);
+      const player = createAudioPlayer();
+
+      player.play(resource);
+      connection.subscribe(player);
+
+      await interaction.reply("✅ Joined the voice channel!");
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+};
