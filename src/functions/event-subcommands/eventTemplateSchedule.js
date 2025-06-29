88404099bfa9f1b0947e3f82639211f679a8a4ec
Brennan McCreary <brennanmccreary@gmail.com>
2025-06-29 19:52:53 -0400

    Fix bug with stage instances not working for event scheduling. Pt.3

--- src/functions/event-subcommands/eventTemplateSchedule.js
+++ src/functions/event-subcommands/eventTemplateSchedule.js
@@ -14,14 +14,14 @@ module.exports = async (client) => {
     const timestamp =
       (await interaction.options.getInteger("timestamp")) ??
       Date.now() + 3600000;
-    
+    const channelData = interaction.guild.channels.cache.get(template.channel);
 
 
     //Check voice channel type
     var channelType;
-    if(channel.type === ChannelType.GuildVoice) {
+    if(channelData.type === ChannelType.GuildVoice) {
       channelType = GuildScheduledEventEntityType.Voice
-    } else if (channel.type === ChannelType.GuildStageVoice) {
+    } else if (channelData.type === ChannelType.GuildStageVoice) {
       channelType = GuildScheduledEventEntityType.StageInstance
     }
 
