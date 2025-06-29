15be5281e4fd0ce450d0b876eac29d2042cda997
Brennan McCreary <brennanmccreary@gmail.com>
2025-06-29 13:45:30 -0400

    Add channel restriction.

--- src/commands/voting/voteForClip.js
+++ src/commands/voting/voteForClip.js
@@ -11,6 +11,20 @@ module.exports = {
     .setName("Cast Vote")
     .setType(ApplicationCommandType.Message),
   async execute(interaction, client) {
+    //Channel restriction
+    try {
+      const ALLOWED_CHANNEL = "1368270934078656613";
+      if (interaction.channelId !== ALLOWED_CHANNEL) {
+        return interaction.reply({
+          content:
+            "❌ You can only use this command in the designated channel.",
+          ephemeral: true,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
     try {
       const message = interaction.targetMessage;
       const user = interaction.user.id;
@@ -22,7 +36,7 @@ module.exports = {
       //     content: `Vote cast!`,
       //     flags: MessageFlags.Ephemeral,
       //   });
-      // } 
+      // }
       //else {
       //   await interaction.reply({
       //     content: `You have already voted for this clip...`,
@@ -31,11 +45,9 @@ module.exports = {
       // }
 
       await interaction.reply({
-          content: `Vote cast!`,
-          flags: MessageFlags.Ephemeral,
-        });
-
-        
+        content: `Vote cast!`,
+        flags: MessageFlags.Ephemeral,
+      });
     } catch (err) {
       console.log(err);
     }
